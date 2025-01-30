79002e39ce55e41eb09aac931efc15b1e92742e8
Ankit Patel <ankitkumarpatel372@gmail.com>
2025-01-30 20:46:53 +0530

    coded signup UI

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.routes.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
 const app = express();
@@ -14,10 +15,18 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log("server is running on PORT:" + PORT);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
